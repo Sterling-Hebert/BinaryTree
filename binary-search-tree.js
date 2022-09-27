ece4a9484f9b74f51db763fe311d517a6384044f
Kelly Kong <kkong1992@gmail.com>
2022-09-27 19:24:36 -0400

    search

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -13,54 +13,63 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  insert(val, currentNode = this.root) {
-    let newNode = new TreeNode(val);
-    // creating new node if root is null
-    if (currentNode === null) {
-      this.root = newNode;
-      return this
-    }
-    // this is creating left node
-    let curr = this.root
-    while(curr){
-      if (val === curr.val) return
-      if (val < curr.val){
-        if(curr.left === null){
-          curr.left = newNode
-          return this
-        }
-        curr = curr.left
-        // this is creating right node
+  // insert(val, currentNode = this.root) {
+  //   let newNode = new TreeNode(val);
+  //   // creating new node if root is null
+  //   if (currentNode === null) {
+  //     this.root = newNode;
+  //     return this
+  //   }
+  //   // this is creating left node
+  //   let curr = this.root
+  //   while(curr){
+  //     if (val === curr.val) return
+  //     if (val < curr.val){
+  //       if(curr.left === null){
+  //         curr.left = newNode
+  //         return this
+  //       }
+  //       curr = curr.left
+  //       // this is creating right node
+  //     } else {
+  //       if (curr.right === null){
+  //         curr.right = newNode
+  //         return this
+  //       }
+  //       curr = curr.right
+  //     }
+  //   }
+  // }
+  insert(val, currentNode=this.root) {
+    let newNode = new TreeNode(val)
+    if(!currentNode) this.root = newNode
+    else if(currentNode.val > val) {
+      if(!currentNode.left) {
+        currentNode.left = newNode
+      } else (
+        this.insert(val, currentNode.left)
+      )
+    } else if (currentNode.val < val) {
+      if(!currentNode.right) {
+        currentNode.right = newNode
       } else {
-        if (curr.right === null){
-          curr.right = newNode
-          return this
-        }
-        curr = curr.right
+        this.insert(val, currentNode.right)
       }
     }
   }
-
   search(val) {
-    // Your code here
-    // if(this.root === null) return false
-    // if (this.root.val === val) return true
-    // if (this.root.left.val === val) return true
-    // if (this.root.right.val === val) return true
-    // if (this.search(this.root.left,val)) return true
-    // return this.search(this.root.right,val)
-    //
-    let currentNode = this.root;
-    while (currentNode) {
-        if (val === currentNode.val) return true;
-        if (val < currentNode.val){
-           currentNode = currentNode.left;
-        }
-        if (val > currentNode.val){
-         currentNode = currentNode.right;
-        }
+    let currentNode = this.root
+
+    while(currentNode) {
+    if(currentNode.val === val) return true;
+
+    if(currentNode.val > val) {
+      currentNode = currentNode.left
+    } else {
+      currentNode = currentNode.right
     }
-    return false;
+    }
+    return false
   }
 
   preOrderTraversal(currentNode = this.root) {
