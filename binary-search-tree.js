daf4ba325f8a897f7fc10836160c4b67ee7f4b5e
Kelly Kong <kkong1992@gmail.com>
2022-09-27 20:11:02 -0400

    post order

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -12,7 +12,6 @@ class BinarySearchTree {
     // Your code here
     this.root = null;
   }
-
   // insert(val, currentNode = this.root) {
   //   let newNode = new TreeNode(val);
   //   // creating new node if root is null
@@ -74,14 +73,27 @@ class BinarySearchTree {
 
   preOrderTraversal(currentNode = this.root) {
     // Your code here
+    if(currentNode === null) return;
+    console.log(currentNode.val);
+    this.preOrderTraversal(currentNode.left);
+    this.preOrderTraversal(currentNode.right)
   }
 
   inOrderTraversal(currentNode = this.root) {
     // Your code here
+    if(currentNode === null) return
+    this.inOrderTraversal(currentNode.left)
+    console.log(currentNode.val)
+    this.inOrderTraversal(currentNode.right)
+
   }
 
   postOrderTraversal(currentNode = this.root) {
     // Your code here
+    if(currentNode === null) return
+    this.postOrderTraversal(currentNode.left)
+    this.postOrderTraversal(currentNode.right)
+    console.log(currentNode.val)
   }
 
   // Breadth First Traversal - Iterative
@@ -92,6 +104,7 @@ class BinarySearchTree {
   // Depth First Traversal - Iterative
   depthFirstTraversal() {
     // your code here
+    if ()
   }
 }
 
